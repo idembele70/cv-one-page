019b6c46ed3042a837faa9b142c405a2090a7192
idembele70 <idembele70@gmail>
2025-10-02 05:11:43 +0200

    feat: v1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { MainCardComponent } from "./components/main-card/main-card.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ SidebarComponent, MainCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cv-one-page';
 }
---
+++ src/app/components/main-card/main-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-card.component.html',
+  styleUrl: './main-card.component.scss'
+})
+export class MainCardComponent {
+
+}
